d424fe74f625e2db7544be8948e2dc18b653de2e
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-18 18:16:13 -0400

    Added the GetPower method to the SuperHuman class

--- COMP123_S2017_LESSON10B/SuperHuman.cs
+++ COMP123_S2017_LESSON10B/SuperHuman.cs
@@ -15,6 +15,7 @@ using System.Threading.Tasks;
  * version: 0.5 - Overridden the built-in toString method
  * version: 0.6 - Added stub Method DisplaySkills to the SuperHuman class to conform to the Human class
  * version: 0.7 - Added private _getPowerIndex method
+ * version: 0.8 - Added the GetPower method
  */
 
 namespace COMP123_S2017_LESSON10B
@@ -133,6 +134,24 @@ namespace COMP123_S2017_LESSON10B
         public override void DisplaySkills()
         {
             Console.WriteLine("Not Implemented!");
+        }
+
+        /// <summary>
+        /// This method returns the power object that matches the Power name.
+        /// If the power is not found it returns an anonymous Power object with
+        /// the name of "Power not found"
+        /// </summary>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        public Power GetPower(string name)
+        {
+            int index = this._getPowerIndex(name);
+            if (index !=-1)
+            {
+                return this.Powers[index];
+            }
+            return new Power("Power not found", 0);
+            
         }
 
     }
