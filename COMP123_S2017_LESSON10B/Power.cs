650d720aebdb3c8ea5b023647bda9cb2ae6ec992
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-07-18 17:52:29 -0400

    Overloaded the > and < operators for the Power class

--- COMP123_S2017_LESSON10B/Power.cs
+++ COMP123_S2017_LESSON10B/Power.cs
@@ -7,9 +7,10 @@ using System.Threading.Tasks;
 /*
  * Name : Sylvere Ekponon
  * Date : July 11, 2017
- * Description : Demo for Lesson 9
+ * Description : Demo for Lesson 10
  * version: 0.1 - Created the Power class
  * version: 0.2 - Added a constructor
+ * version: 0.3 - Overloaded the > and < operators for the Power class 
  */
 
 namespace COMP123_S2017_LESSON10B
@@ -36,6 +37,29 @@ namespace COMP123_S2017_LESSON10B
         {
             this.Name = name;
             this.Rank = rank;
+        }
+
+        //PUBLIC OVERLOADED OPERATORS
+        /// <summary>
+        /// This method overloads the > operator for use in the Power class
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns>returns true is lhs.Rank > rhs.Rank</returns>
+        public static bool operator >(Power lhs, Power rhs)
+        {
+            return (lhs.Rank > rhs.Rank);
+        }
+
+        /// <summary>
+        /// This method overloads the < operator for use in the Power class
+        /// </summary>
+        /// <param name="lhs"></param>
+        /// <param name="rhs"></param>
+        /// <returns>returns true is lhs.Rank < rhs.Rank</returns>
+        public static bool operator <(Power lhs, Power rhs)
+        {
+            return (lhs.Rank < rhs.Rank);
         }
 
     }
